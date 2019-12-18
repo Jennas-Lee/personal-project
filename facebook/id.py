291c88476cc010f61afa05c7ac00b0cf02cb551d
iamseungjun <leezeunjun@gmail.com>
2019-12-18 23:13:55 +0900

    Dec 18, 2019

--- facebook/id.py
+++ facebook/id.py
@@ -8,12 +8,14 @@ import json
 def main():
     #[CODE 1]
     page_name = "jtbcnews"
-    app_id = ""     # App ID
-    app_secret = "" # App Secret CODE
+    app_id = "559747767922395"     # App ID
+    app_secret = "c1ae39404313ecdd2e7d7d55bd9a0280" # App Secret CODE
     access_token = app_id + "|" + app_secret
+    # EAAH9Fm7e7tsBACSbOrVmpZB7fZBXW7kzlmxVHSAKfQTnMTxfA6p40LBgqAY16PR8qYUmc9mhOZBxxFYGs1gaUqhmQ3KXat1DEzGbqNMnr0qfQQScXbHLsigV59DZAJ9tSfeN70Vue7ZA1xiO7OfkTXiJbTEwXJMwT1V8LoZC6gJQJAECXZBtY6U4lGoQ3ZAhIvvJSVFAMR2IEANMpA14x13wW8oRGRj2Ao4ZD
+    # 200920440387013 jtbcnews code
 
     #[CODE 2]
-    base = "https://graph.facebook.com/v2.8"    # CHECK VERSION
+    base = "https://graph.facebook.com/v2.9"    # CHECK VERSION
     node = "/" + page_name
     parameters = "/?access_token=%s" % access_token
     url = base + node + parameters
@@ -25,4 +27,12 @@ def main():
     try:
         response = urllib.request.urlopen(req)
         if response.getcode() == 200:
-            data = 
+            data = json.loads(response.read().decode('utf-8'))
+            page_id = data['id']
+            print("%s Facebook Numeric Number ID : %s" % (page_name, page_id))
+    except Exception as e:
+        print(e)
+        print(url)
+
+if __name__ == "__main__":
+    main()
