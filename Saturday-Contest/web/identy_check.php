2c1708a48ee69f46bc6d8e157d8165d2698f7b5d
Jennas-Lee <leezeunjun@gmail.com>
2021-02-20 16:43:38 +0900

    Detete SNC Files

--- Saturday-Contest/web/identy_check.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include "db.php";
-    if(isset($_POST) && $_SERVER['REQUEST_METHOD'] == "POST"){
-        @extract($_POST);
-        function userexist
-    }
-?>
--- Saturday-Contest/web/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    // session_start();
-    session_destroy();
-?>
-<meta charset="utf-8">
-<script>location.href="index.php";</script>
